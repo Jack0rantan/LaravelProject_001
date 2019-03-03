1811a23122bef6481d7f07e44e0fff55189269a9
k <kankan052@gmail.com>
2019-03-04 01:25:08 +0900

    [add & revise] head.blade.php
    update head.balde.php, google.blade.php, font.blade.php

---
+++ resources/views/layout/font.blade.php
@@ -0,0 +1,29 @@
+@section ('font')
+<!--********************************************************************************************************************************-->
+<!-- web font -->
+ 
+<link href="https://fonts.googleapis.com/css?family=Miss+Fajardose" rel="stylesheet">
+<style>
+  .font-MF{
+     font-family: 'Miss Fajardose', cursive;
+  }
+</style>
+<link href="https://fonts.googleapis.com/css?family=Noto+Sans+TC" rel="stylesheet">
+<style>
+  .font-Noto{
+    font-family: 'Noto Sans TC', sans-serif;
+  }
+</style>
+<link href="https://fonts.googleapis.com/css?family=Sawarabi+Mincho" rel="stylesheet">
+<style>
+  .font-Sawarabi{
+    font-family: 'Sawarabi Mincho', sans-serif;
+  }
+</style>
+<link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
+<style>
+  .font-Osward{
+    font-family: 'Oswald', sans-serif;
+  }
+</style>
+@endsection
--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -1,4 +1,9 @@
 @section ('head')
+
+  <!-- Google Analytics -->
+  @yield ('google')
+
+  <!--  -->
   <meta charset = "UTF-8">
   <title>@yield('title')</title>
 
@@ -6,13 +11,17 @@
   <meta name = "description" itemprop = "descriotion" content = "@yield('description')">
   <meta name = "keywords" itemprop = "keywords" content = "@yield('keywords')">
 
-  <!-- for Bootstrap -->
+  <!-- for Bootstrap & jQuery -->
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet" media="screen">
-  <link href="/css/sticky-footer.css" rel="stylesheet" media="screen">
 
-  <!-- if necessary 
-  <link href = "/css/layout.css" rel = "stylesheet">
-  @yield ('pageCss') -->
-
+<!--********************************************************************************************************************************-->
+  <!-- JavaScript -->
+<script src="{{ asset('/js/main.min.js') }}"></script>
+<!--********************************************************************************************************************************-->
+  <!-- css読み込み -->
+<link rel="stylesheet" href="{{ asset('/css/main.css') }}" type="text/css">
+<!--********************************************************************************************************************************-->
+  <!-- font -->
+  @yield ('font')
 @endsection
