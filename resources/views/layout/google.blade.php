761379670e393c984f14dcd28445a434eddf16d9
k <kankan052@gmail.com>
2019-04-04 09:25:04 +0900

    [add] Google Adsense

--- resources/views/layout/google.blade.php
+++ resources/views/layout/google.blade.php
@@ -10,4 +10,13 @@
       gtag('config', 'UA-134041531-1');
     </script>
 
+
+  <!-- for Google Adsense -->
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script>
+      (adsbygoogle = window.adsbygoogle || []).push({
+        google_ad_client: "ca-pub-5531776326914830",
+        enable_page_level_ads: true
+      });
+    </script>
 @endsection
