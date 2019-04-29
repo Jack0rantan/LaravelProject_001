f1b72af018001aa09f734b77333c1bfa6ae803a1
k <kankan052@gmail.com>
2019-04-30 05:24:50 +0900

    [add] English Movie page

--- resources/views/layout/english/english-movie-site.blade.php
+++ resources/views/layout/english/english-movie-site.blade.php
@@ -41,8 +41,8 @@
 
                 <div class="col-12 row"></div>
 
-              <p>英語学習で重要なのは”いかにアウトプットするか”、が私の持論です。</p>
-              <p>上記のような悩みを持つ学生あるいは社会人の皆さんには是非とも以下で紹介するサイトを使って実践的な知識のインプット・アウトプットにご活用ください。</p>
+              <p>英語学習では”インプットした知識をいかにアウトプットするか”が重要である、が私の持論です。</p>
+              <p>上記のような悩みを持つ学生あるいは社会人の皆さんには是非とも以下で紹介するサイトを使って実践的な知識をインプットあるいはシャドーイングや暗唱をしてアウトプットするなどご活用いただき、スキルアップしてください。</p>
               <p></p>
             </div>
           </div>
@@ -51,31 +51,203 @@
           <!-- 目次 -->
           <div class="row justify-content-center">
             <div class="index font-KosugiMaru">
-              <ul style="list-style:none">
-                <div class="margin002"></div>
-                <h3 class="indexTxt">目次</h3>
-                <li> 1. 役割</li>
-                <li>   1-1. 目的 </li>
-                <li>   1-2. 事例 </li>
-                <li> 2. middelwareの作成・登録</li>
-                <li>   2-1. 作成</li>
-                <li>   2-2. 登録</li>
+              <h3 class="indexTxt">目次</h3>
+              <ul style="list-style-type:none">
+                <li> 1. おすすめ動画サイト</li>
+                <ul style="list-style-type:none">
+                  <li>   1-1. TED </li>
+                  <li>   1-2. CNN</li>
+                  <li>   1-3. YouTube</li>
+                  <li>   1-4. Hulu</li>
+                  <li>   1-5. Twitch</li>
+            <div class="margin002 col-12 row"></div>
+                </ul>
+                <li> 2. 勉強方法の提案</li>
+                <ul style="list-style-type:none">
+                  <li>   2-1. スクリプトを読む</li>
+                  <li>   2-2. シャドーイング</li>
+                  <li>   2-3. 暗唱・独り言</li>
+                </ul>
                 <li> 3. まとめ</li>
-                <li> 参考文献 </li>
               </ul>
             </div>
           </div>
 
+          <!-- margin -->
+          <div class="margin002 col-sm-12 row"></div>
+
+          <!-- 目次 -->
+          <h2 class="artTag1">1. おすすめ動画サイト</h2>
+          <h3 class="artTag2">1-1. TED</h3>
+
+          <div class="artTag3-t">
+
+            <img src="/img/English/TED_menu.png" alt="TED-image" class="img-responsive center-block">
+
+            <div class="margin003 col-12 row"></div>
+
+            <p>先ずは何と言ってもこのサイトを紹介しないわけにはいきません。</p>
+            <p>言わずと知れた非営利団体Technology Entertainment Design、通称TED。</p>
+            <p>非常に多くの著名人や世界各地で様々な活躍を見せる方々がプレゼンテーションを披露していますね。</p>
+            <p>以下のような書物もあり、店頭で手に取った方も多いのではないでしょうか？</p>
+            <ul>
+              <li>「TEDに学ぶ最強のプレゼン術」（アカッシュ・カリア著、月沢李歌子訳、SB文庫）</li>
+              <li>「TED 驚異のプレゼン 人を惹きつけ、心を動かす9つの法則 」（マーカイン・ガロ著、土方奈美訳、日経BP社）</li>
+            </ul>
+            <p>特にこのサイトが優れているのはスクリプトがどの記事に対しても用意されており、さらに動画のどこを再生しているかが明示的にわかるようになっています</p>
+            <p>またカテゴリーや、動画の長さからも検索できるようになっており、</p>
+            <p>例えば、短めの動画を見たいなあ、という際には durationから0~6を選択するといった検索も可能で、自分のニーズに合う動画を検索することが可能です。</p>
+            <img src="/img/English/TED_search.png" alt="TED-search-duration" class="img-responsive center-block">
+
+            <a href="https://www.ted.com/#/">TED</a>
+            <a href="https://www.ted.com/talks">TED Talks</a>
+            <div class="margin005 col-12 row"></div>
+
+          </div>
+
+          <h3 class="artTag2">1-2. CNN</h3>
+          <div class="artTag3-t">
+
+            <img src="/img/English/CNN_menu.png" alt="CNN-image" class="img-responsive center-block">
+
+            <p>CNN : Cable News Netowrkの略称。</p>
+            <p>何と言っても特徴は世界中のニュースを24時間放送していること。</p>
+            <p>動画だけではなく普通のニュース記事もあるため、通勤の電車の中では動画が見られない、、という場合でも気軽に英語に触れられることがメリットといえるでしょう。</p>
+            <p>そして何と言っても、<a href="https://edition.cnn.com/specials/asia/on-japan">On Japan</a>では日本のニュースを英語で閲覧することが可能です！</p>
+            <p>身近なニュースなので「そもそも何言ってるんだ？」という疑問でリズムを損なわずに英語に触れることができるでしょう。</p>
+            <p>勉強においてリズムは集中力を維持するにあたり非常に重要だと考えます。集中できれば生産性に富んだ学習を体験できるはずです。</p>
+            <p>ぜひニュースを英語で読むというのを習慣化し、スキルアップしましょう！</p>
+
+            <li><a href="https://edition.cnn.com/">CNN</a></li>
+            <li><a href="https://edition.cnn.com/specials/asia/on-japan">CNN On Japan</a></li>
+
+            <div class="margin005 col-12 row"></div>
+          </div>
+
+          <h3 class="artTag2">1-3. YouTube</h3>
+          <div class="artTag3-t">
+
+            <img src="/img/English/YouTube.png" alt="YouTube-image" class="img-responsive center-block">
+
+            <div class="margin004 col-12 row"></div>
+
+            <p>紹介するまでもない、今やインフラといっても過言でもないビッグサイト、YouTubeです。</p>
+            <p>多種多様なYouTuberが英語における学習動画を配信していたり、もちろん海外の方の動画を閲覧すればそれ自体が良い学習教材となるでしょう。</p>
+            <p>特徴を挙げるとすれば自分の趣味や興味のあることをベースに動画を検索することが可能なので、</p>
+            <p>「勉強するぞ」と意気込む必要なく、持続的な学習効果が望めるでしょう。</p>
+            <p>デメリットとしてはエンターテイメントに富む動画が多いので、ついついそちらに興味を惹かれて英語学習に集中できないなどの懸念が潜在していることでしょうか。</p>
+            <p>是非とも誘惑にうちがって集中して取り組んでいただければと思います。</p>
+            <p>一つだけチップスを紹介したいと思います。</p>
+            <p>Youtubeは通常地域が日本に設定されていますが、アメリカ版に変更可能です。</p>
+            <p>1.page上部にある自分のアカウントアイコンをクリック。(以下でいう紫色の人)</p>
+            <img style="max-width:50%" src="/img/English/YouTube_location_3.png" alt="Youtube-locatino-config" class="img-responsive center-block">
+            <div class="margin002 col-12 row"></div>
+            <p>2."場所：日本" を選択</p>
+            <img style="max-width:50%" src="/img/English/YouTube_location_1.png" alt="Youtube-location-config" class="img-responsive center-block">
+            <div class="margin002 col-12 row"></div>
+            <p>3. "アメリカ"を選択</p>
+            <img style="max-width:50%" src="/img/English/YouTube_location_2.png" alt="Youtube-location-config" class="img-responsive center-block">
+            <div class="margin002 col-12 row"></div>
+            <p>これだけでアメリカ版で検索することが可能です。急上昇を見るとその地域における流行などを知ることができるでしょう。</p>
+
+            <a href="https://www.youtube.com/">YouTube</a>
+
+            <div class="margin005 col-12 row"></div>
+          </div>
+
+          <h3 class="artTag2">1-4. Hulu</h3>
+          <div class="artTag3-t">
+
+            <img src="/img/English/Hulu.png" alt="Hulu-image" class="img-responsive center-block">
+
+            <p>海外のドラマを英語字幕付きで見ることができる動画配信サービス、Hulu。</p>
+            <p>ここまで紹介してきたサイトと違い有料ではある点がネックではあります、、が！</p>
+            <p>しかし、ドラマとして楽しむことができるためこれまで紹介した動画サイト同様、飽きずに楽しむことができます！</p>
+            <p>AmazonPrimeなどでも動画を視聴することは可能ではありますが、英語字幕が完備されているとは言えず、その点でこのサイトを紹介することにしました。</p>
+            <p>さらに10秒巻き戻しや、30秒巻き戻しが可能なため、これら機能を使ってシャドーイングやリスニングも鍛えることが可能である点が非常に有益です！</p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <a href="https://www.happyon.jp/">Hulu</a>
+            <div class="margin005 col-12 row"></div>
+
+          </div>
+
+          <h3 class="artTag2">1-5. Twitch</h3>
+          <div class="artTag3-t">
+
+            <img src="/img/English/Twitch.png" alt="Twitch-image" class="img-responsive center-block">
+
+            <p>海外ゲーマ御用たちの動画配信サイト、Twitch。</p>
+            <p>YouTubeとは違いオンタイムに海外の配信者が言葉を発しているので、英会話をチャットやスカイプなどのサービスを通じて英会話することも可能です。</p>
+            <p>何かを学習するのに長けているサイトではないかもしれないがリアルな会話に身をおくことができ、友人関係も築ける可能性を秘めています。</p>
+            <p>日本にいても海外の知り合いを作ってしまえば留学と大差ない環境まで持ち込むことも可能です。</p>
+            <p>TVゲームだけではなく、スマホゲームやチェスなどのボードゲームもカテゴリされていて人口も多いため、何かしら興味のある分野を検索・閲覧してみるのも良いきっかけになるのではないでしょうか？</p>
+            <a href="https://www.happyon.jp/">Hulu</a>
+            <div class="margin005 col-12 row"></div>
+
+          </div>
 
           <!-- margin -->
-          <div class="col-sm-12 row">
-            <div class="margin002"></div>
+          <h3 class="artTag1">2. 勉強方法の提案</h3>
+          <div class="artTag3-t">
+            <p>ここではTEDを扱った勉強方法に特化して説明したいと思います。</p>
+            <p>特に今回は以下動画を扱って解説します。</p>
+            <a href="https://www.ted.com/talks/damian_palin_mining_minerals_from_seawater?language=en">Mining mineral from seawater</a>
+
+            <div class="margin005 col-12 row"></div>
+
           </div>
 
-          <!-- contents  -->
-          <div class="col-sm-12">
-            <div class="margin004"></div>
+          <h3 class="artTag2">2-1. シャドーイング</h3>
+
+          <div class="artTag3-t">
+            <p>先ずは動画を視聴し、聞き取った音声をそのまま発音しましょう（=シャドーイング）。</p>
+            <p>シャドーイングでよくわからない単語などがある場合には字幕をつけて、聞き取りながら発生し、再度シャドーイングすると良いと思います。</p>
+            <p>字幕は動画下部にある吹き出しマークをクリックし、Englishを選択することで見ることができます。</p>
+            <p>日本語字幕をつけて意味を理解したい、という場合は日本語を。ただし動画によっては用意されていないケースもあるため視聴前にチェックしておきましょう。</p>
+            <img src="/img/English/TED_method_3.png" alt="Hulu-image" class="img-responsive center-block">
+            <div class="margin005 col-12 row"></div>
+
           </div>
+
+          <h2 class="artTag2">2-2. スクリプトを読む</h2>
+
+          <div class="artTag3-t">
+            <p>スクリプトは動画視聴後の復習用に扱うことを提案したいです。</p>
+            <p>動画は刻々とシーンが展開されるため、落ち着いて文章や文法、ストーリの前後関係を整理しにくい場合があるかもしれません。視聴しながら意味理解がスムーズに行くのであればそれに越したことはないのですが、そうもいかない場合は多々あると思います。</p>
+            <p>その際はぜひスクリプトを参照し、辞書を引いたり、動画再生されると良いかと思います。</p>
+            <p>では、スクリプトですが、動画下部の"Transcript"をクリックしましょう。</p>
+            <img src="/img/English/TED_method_1.png" alt="Hulu-image" class="img-responsive center-block">
+            <div class="margin003 col-12 row"></div>
+
+            <p>すると以下のようにスクリプトが出てくると思います。動画を同時に視聴すると以下のように再生箇所がハイライトされるため、目で追いやすくなります。</p>
+            <img src="/img/English/TED_method_2.png" alt="Hulu-image" class="img-responsive center-block">
+
+            <div class="margin005 col-12 row"></div>
+          </div>
+
+          <h3 class="artTag2">2-3. アウトプットする（暗唱・独り言・英会話など）</h3>
+          <div class="artTag3-t">
+            <p>最後に、上記で動画の内容を覚えたら暗唱したり、フレーズを抽出・あるいはその文章を独り言のようにつぶやくなどして知識をアウトプットしましょう。</p>
+            <p>もちろん、英会話で実践するのも理想的です。</p>
+            <p>ぜひ英語力を上達させましょう！</p>
+
+          </div>
+
+          <h2 class="artTag1">3. まとめ</h2>
+          <div class="artTag3-t">
+            <p>いかがだったでしょうか？</p>
+            <p>ネイティブの発音はTOEICや英語教材の発音ほど綺麗でない場合はあると思います。がそれが実践です。</p>
+            <p>発音を正しく覚えることは重要ですが、正しい綺麗な発音しか聞き取れないのは問題です。いつでも理想な環境が用意されているわけではないですからね。</p>
+            <p>上記のようにツールは揃っています。初めは短めの動画を選ぶと良いと思います。ぜひ時間と量をかけて経験値を持続的に増やしていきましょう！</p>
+            <p>そうすれば必ず力はつきます！頑張りましょう！</p>
+            <p>それではこの辺で。お問い合わせなどは<a href="mailto:sakura.rantan&#64;gmail.com">こちら</a>。</p>
+          </div>
+
+          <!-- margin -->
+          <div class="margin004 col-sm-12 row"></div>
 
         </div>
         <div class="col-sm-1"></div>
--- resources/views/page/english/english-movie-site.blade.php
+++ resources/views/page/english/english-movie-site.blade.php
@@ -3,7 +3,7 @@
 
 <!-- SEO -->
 @section ('title', '【厳選５選！】おすすめ英語動画サイト')
-@section ('keywords','英語勉強法,TED')
+@section ('keywords','英語勉強法,TED,CNN,独学,英語学習,英会話')
 @section ('descriotion','英語学習に最適な動画サイトを紹介します。')
 
 <!-- head -->
