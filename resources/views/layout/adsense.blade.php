9fbc2020f3407081255ee4f7bdb81ae534568ce2
k <kankan052@gmail.com>
2019-05-21 12:39:24 +0900

    [revise] adsense

--- resources/views/layout/adsense.blade.php
+++ resources/views/layout/adsense.blade.php
@@ -1,10 +1,10 @@
 @section ('adsense')
-  <!-- for Google Adsense -->
-    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <script>
-      (adsbygoogle = window.adsbygoogle || []).push({
-        google_ad_client: "ca-pub-5531776326914830",
-        enable_page_level_ads: true
-      });
-    </script>
+<!--for Adsense-->
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-5531776326914830",
+    enable_page_level_ads: true
+  });
+</script>
 @endsection
